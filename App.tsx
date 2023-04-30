15b6bd4edebbc27d724911345cc7cb7bbe59c0af
Hanzalausman164 <muhammadhanzala164@gmail.com>
2023-04-30 21:47:45 +0500

    first commit

---
+++ App.tsx
@@ -0,0 +1,43 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import React from 'react';
+import type {PropsWithChildren} from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+
+import {
+  Colors,
+  DebugInstructions,
+  Header,
+  LearnMoreLinks,
+  ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
+
+
+
+function App(): JSX.Element {
+  
+
+  return (
+    <View>
+      <Text>
+         Hello chomu developers
+      </Text>
+    </View>
+  );
+}
+
+
+export default App;
